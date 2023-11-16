b4df8cd5b6e45fbf9ad9b42bf865b0e82d878f0c
OvsyannikovIA <kokos.demon.bit@gmail.com>
2023-11-16 20:33:22 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,3 @@
+name = input('Введите своё имя')
+print(f'Hello {name}')
+input('kiss my ass')
---
+++ test2.py
@@ -0,0 +1,5 @@
+from selene.support.shared import browser
+
+
+def test_open_browser():
+    browser.open('https://learngitbranching.js.org/?locale=ru_RU')
